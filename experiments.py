9800ac21e96002e162684bacfec072fa32929420
Constantin Kogler <constantin.kogler@gmail.com>
2023-05-28 18:59:32 +0100

    Minor updates

--- experiments.py
+++ experiments.py
@@ -2,9 +2,9 @@ import groups as gp
 import cayleygraphs as cg
 import LPS
 
-for k in range(20,100):
+for k in range(15,100):
     G = gp.SL(2,k)
-    set = [(1,1,0,1), (1,0,1,1)]
+    set = [(1,2,0,1), (1,0,2,1)]
     Cay = cg.CayleyGraph(G,set)
     noe = len(Cay.group.elements)
     asg = cg.adjacency_spectral_gap(Cay)
