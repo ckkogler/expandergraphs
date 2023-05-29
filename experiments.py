8e96b106f0a699738bc477d24a98f00fdae1f525
Constantin Kogler <constantinkogler@gamil.com>
2023-05-29 10:52:33 +0000

    Some chages in the experiment file.

--- experiments.py
+++ experiments.py
@@ -1,8 +1,9 @@
+import numpy as np
 import groups as gp
 import cayleygraphs as cg
 import LPS
 
-for k in range(15,100):
+for k in range(3,16):
     G = gp.SL(2,k)
     set = [(1,2,0,1), (1,0,2,1)]
     Cay = cg.CayleyGraph(G,set)
@@ -10,4 +11,5 @@ for k in range(15,100):
     asg = cg.adjacency_spectral_gap(Cay)
     lsg = cg.laplacian_spectral_gap(Cay)
     gth = cg.girth(Cay)
-    print(f"k = {k}, noe = {noe}, girth = {gth}, asg = {asg}, lsg = {lsg}")
+    gthratio = gth*np.log(3)/np.log(noe)
+    print(f"k = {k}, noe = {noe}, girth = {gth}, asg = {asg}, lsg = {lsg}, gthrat = {gthratio}")
